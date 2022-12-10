b25cbbecb8ee2619d6307679e90909d954b7bb1e
rk-42 <66195133+rk-42@users.noreply.github.com>
2022-12-10 18:27:31 +0100

    Delete day02 directory

--- day02/ex00/ft_filter.py
+++
@@ -1,7 +0,0 @@
-def ft_filter(function_to_apply, list_of_inputs):
-    if not list_of_inputs:
-        return []
-    if function_to_apply is not None:
-        return [x for x in list_of_inputs if function_to_apply(x)]
-    elif function_to_apply is None:
-        return [x for x in list_of_inputs if function_to_apply is None]
--- day02/ex00/ft_map.py
+++
@@ -1,5 +0,0 @@
-def ft_map(function_to_apply, list_of_inputs):
-    result = []
-    for x in list_of_inputs:
-        result.append(function_to_apply(x))
-    return result
